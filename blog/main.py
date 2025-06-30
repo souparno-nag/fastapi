9e29bc9fca9ab015c49561c1e5da1855159358d1
souparno-nag <souparnonag2004@gmail.com>
2025-06-30 19:39:33 +0530

    users added

--- blog/main.py
+++ blog/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, Depends, status, Response, HTTPException
 # from pydantic import BaseModel
-from . import schemas, models
+from . import schemas, models, hashing
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session 
 from typing import List
+from passlib.context import CryptContext
 
 app = FastAPI()
 
@@ -37,7 +38,7 @@ def get_db():
         db.close()
 
 # @app.post('/blog', status_code=201)
-@app.post('/blog', status_code=status.HTTP_201_CREATED)
+@app.post('/blog', status_code=status.HTTP_201_CREATED, tags=['blogs'])
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     # return db
     new_blog = models.Blog(title = request.title, body=request.body)
@@ -46,7 +47,7 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=['blogs'])
 def destroy(id: int, db: Session = Depends(get_db)):
     # blog = db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
     blog = db.query(models.Blog).filter(models.Blog.id == id)
@@ -56,7 +57,7 @@ def destroy(id: int, db: Session = Depends(get_db)):
     db.commit()
     return 'done'
 
-@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED, tags=['blogs'])
 def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
     # pass
     # db.query(models.Blog).filter(models.Blog.id == id).update({'title':'Updated title'})
@@ -68,12 +69,12 @@ def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
     db.commit()
     return 'updated'
 
-@app.get('/blog', status_code=status.HTTP_200_OK, response_model=List[schemas.ShowBlog])
+@app.get('/blog', status_code=status.HTTP_200_OK, response_model=List[schemas.ShowBlog], tags=['blogs'])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
+@app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog, tags=['blogs'])
 def show(id: int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -82,3 +83,24 @@ def show(id: int, response: Response, db: Session = Depends(get_db)):
         return {'detail': f'Blog with the id {id} not available'}
     return blog
 
+pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+@app.post('/user', response_model=schemas.ShowUser, tags=['users'])
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    # return request
+    # new_user = models.User(name = request.name, email=request.email, password=request.password)
+
+    # hashedPassword = pwd_cxt.hash(request.password)
+    # new_user = models.User(name = request.name, email=request.email, password=hashedPassword)
+    new_user = models.User(name = request.name, email=request.email, password=hashing.Hash.bcrypt(request.password))
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@app.get('/user/{id}', response_model=schemas.ShowUser, tags=['users'])
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with the id {id} not found')
+    return user
--- blog/schemas.py
+++ blog/schemas.py
@@ -5,8 +5,21 @@ class Blog(BaseModel):
     body: str
 
 class ShowBlog(Blog):
+    title: str
+    body: str
     # pass
-    class Config:
+    class Config():
         orm_mode = True  # This allows Pydantic to read data from ORM models
         # It tells Pydantic to treat the SQLAlchemy model as a dictionary
         # and convert it to a Pydantic model.
+
+class User(BaseModel):
+    name: str
+    email:str
+    passsword: str
+
+class ShowUser(BaseModel):
+    name: str
+    email:str
+    class Config():
+        orm_mode = True
